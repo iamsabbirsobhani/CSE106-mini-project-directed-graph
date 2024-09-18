ebc68f771bbee4bd3b5c6c2186795725c75a7a8a
iamsabbirsobhani <sabbirsobhani@gmail.com>
2024-09-19 01:48:59 +0600

    done project q 2: sum of InD eq sum of OuD

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 int main() {
     int sumOfOutDegree = 0;
     int sumOfInDegree = 0;
+    int totalSumOfInDegrees = 0;
+    int totalSumOfOutDegrees = 0;
     // Part II Presentation: Slide page 96, Definition 4, Example:
     int directed_graph_array[7][7] = {
             {1, 1, 1, 0, 1, 0, 0},
@@ -24,9 +26,19 @@ int main() {
             }
         }
         printf("deg-(%d) = %d\n", i, sumOfInDegree);
+        totalSumOfInDegrees += sumOfInDegree;
         printf("deg+(%d) = %d\n", i, sumOfOutDegree);
+        totalSumOfOutDegrees += sumOfOutDegree;
         sumOfOutDegree = 0;
         sumOfInDegree = 0;
+    }
+
+    printf("Total Sum of In Degrees: %d\n", totalSumOfInDegrees);
+    printf("Total Sum of Out Degrees: %d\n", totalSumOfOutDegrees);
+    if (totalSumOfInDegrees == totalSumOfOutDegrees) {
+        printf("Sum of in-degrees and Sum of out-degrees are equal.\n");
+    } else {
+        printf("Sum of in-degrees and Sum of out-degrees are not equal!");
     }
     return 0;
 }
